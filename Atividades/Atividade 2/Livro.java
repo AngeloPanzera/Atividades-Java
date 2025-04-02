dac5db82b3860ea7729f0cb833febba5f9d345e3
Ângelo Panzera <angelopmata1@gmail.com>
2025-04-02 01:10:26 -0300

    Script em java de uma biblioteca pessoal.

---
+++ Atividades/Atividade 2/Livro.java
@@ -0,0 +1,28 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int ano;
+
+    public Livro(String titulo, String autor, int ano) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    public void exibirInfo() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Ano de Publicação: " + ano);
+    }
+
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("Dom Casmurro", "Machado de Assis", 1899);
+        Livro livro2 = new Livro("1984", "George Orwell", 1949);
+
+        System.out.println("Informações do primeiro livro:");
+        livro1.exibirInfo();
+
+        System.out.println("\nInformações do segundo livro:");
+        livro2.exibirInfo();
+    }
+}
