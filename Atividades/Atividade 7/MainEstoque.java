1cc905ab4be26335a0255c33608a54ceb01713d5
Felipe Furtuna <148819797+FelipeFurtuna@users.noreply.github.com>
2025-04-02 10:23:53 -0300

    um sistema simples de gerenciamento de estoque

--- Atividades/Atividade 7/MainEstoque.java
+++ Atividades/Atividade 7/MainEstoque.java
@@ -2,7 +2,7 @@ public class MainEstoque {
     public static void main(String[] args) {
         Estoque estoque = new Estoque();
 
-       
+        
         estoque.adicionarProduto("Arroz", "001", 20.0, 10);
         estoque.adicionarProduto("Feijão", "002", 8.5, 20);
         estoque.adicionarProduto("Macarrão", "003", 5.0, 15);
