f2c49f4e838e6168cf6497f0ef6a378932b302a2
Felipe Furtuna <148819797+FelipeFurtuna@users.noreply.github.com>
2025-04-02 10:22:02 -0300

    Update MainEstoque.java

--- Atividades/Atividade 7/MainEstoque.java
+++ Atividades/Atividade 7/MainEstoque.java
@@ -2,15 +2,15 @@ public class MainEstoque {
     public static void main(String[] args) {
         Estoque estoque = new Estoque();
 
-        // Adicionando produtos ao estoque
+       
         estoque.adicionarProduto("Arroz", "001", 20.0, 10);
         estoque.adicionarProduto("Feijão", "002", 8.5, 20);
         estoque.adicionarProduto("Macarrão", "003", 5.0, 15);
 
-        // Exibindo todos os produtos no estoque
+       
         estoque.listarProdutos();
 
-        // Atualizando um produto
+        
         System.out.println("\nAtualizando produto...");
         estoque.removerProduto("002");
 
