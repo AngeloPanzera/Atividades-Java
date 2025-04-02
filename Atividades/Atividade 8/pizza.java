c28573885a2dcafb214e969a7bdee62af46ae73d
Felipe Furtuna <148819797+FelipeFurtuna@users.noreply.github.com>
2025-04-02 09:45:18 -0300

    O código implementa um sistema simples de gerenciamento de pedidos de pizza, permitindo registrar pedidos, calcular preços e exibir detalhes da compra.

---
+++ Atividades/Atividade 8/pizza.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+class Pizza {
+    private String nome;
+    private double preco;
+    private List<String> ingredientes;
+
+    public Pizza(String nome, double preco, List<String> ingredientes) {
+        this.nome = nome;
+        this.preco = preco;
+        this.ingredientes = ingredientes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void exibirPizza() {
+        System.out.println("Pizza: " + nome + " | Preço: R$ " + preco);
+        System.out.println("Ingredientes: " + ingredientes);
+    }
+}
+
